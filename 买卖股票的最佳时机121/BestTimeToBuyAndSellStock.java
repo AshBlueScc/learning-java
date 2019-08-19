5814a22bb3835459287548fb7acd9b928a8da6d9
runoob <test@123.com>
2019-08-19 18:13:09 +0800

    1.create the project 2.complete some problems

--- 买卖股票的最佳时机121/BestTimeToBuyAndSellStock.java
+++ 买卖股票的最佳时机121/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,35 @@
 package 买卖股票的最佳时机121;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String sa = sc.nextLine();
+        String[] sa2 = sa.replace("[", "").replace("]", "").replace(" ","").split(",");
+        System.out.println(Arrays.toString(sa2));
+        int[] array = new int[sa2.length];
+        for(int i = 0; i < sa2.length; i++){
+            array[i] = Integer.parseInt(sa2[i]);
+        }
+        System.out.println(maxProfit(array));
+
+    }
+
+        private static int maxProfit(int[] prices) {
+            int max = 0;
+            for(int i = 0; i < prices.length - 1; i++){
+                for(int j = i + 1; j < prices.length; j++){
+                    int profit = prices[j] - prices[i];
+                    if(profit > max){
+                        max = profit;
+                    }
+                }
+            }
+            return max > 0 ? max:0;
+        }
+
+
 }
