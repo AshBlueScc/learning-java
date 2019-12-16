51ccea8954994f925d5d003f8fb3018a4f8d1f56
chengzhen <chengzhen@bw30.com>
2019-12-16 17:46:39 +0800

    JAVA并发编程的艺术第二章

---
+++ 多线程到底层/java并发编程的艺术/chapter2_Java并发机制的底层实现原理/examples/Counter.java
@@ -0,0 +1,65 @@
+package chapter2_Java并发机制的底层实现原理.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created By ChengZhen on 2019/12/16.
+ * Author: Ash
+ * Date: 2019/12/16
+ * Time: 16:53
+ * Debug the codes and debug the world!
+ */
+public class Counter {
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    private int i = 0;
+
+    public static void main(String[] args) {
+        final Counter cas = new Counter();
+        List<Thread> ts = new ArrayList<Thread>(600);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 10000; j++) {
+                        cas.count();
+                        cas.safrCount();
+                    }
+                }
+            });
+            ts.add(t);
+        }
+
+        for (Thread t : ts) {
+            t.start();
+        }
+        //等待所有线程执行完成
+        for (Thread t : ts) {
+            try {
+                t.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println(cas.i);
+        System.out.println(cas.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    /** *使用CAS实现线程安全计数器 */
+    private void safrCount() {
+        for (;;){
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+
+    private void count() {
+        i++;
+    }
+}
