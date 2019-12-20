81442fa45f45623a14d3053d48dd942c58eb6ecb
chengzhen <chengzhen@bw30.com>
2019-12-20 17:32:37 +0800

    little change

--- 多线程到底层/java并发编程的艺术/chapter2_Java并发机制的底层实现原理/examples/Test.java
+++ 多线程到底层/java并发编程的艺术/chapter2_Java并发机制的底层实现原理/examples/Test.java
@@ -8,17 +8,32 @@ package chapter2_Java并发机制的底层实现原理.examples;
  * Debug the codes and debug the world!
  */
 public class Test {
-    private static int k = 0;
-    private static int l = 0;
+//    private static int k = 0;
+//    private static int l = 0;
+//
+//    public static void main(String[] args) {
+//        //i++,和++i的区别
+//        //i++先做别的再++；++i先加再作别的
+//        for (int i = 0; i < 10; i++) {
+//            k = k++;
+//            l = ++l;
+//        }
+//        System.out.println(k);
+//        System.out.println(l);
+//    }
 
-    public static void main(String[] args) {
-        //i++,和++i的区别
-        //i++先做别的再++；++i先加再作别的
-        for (int i = 0; i < 10; i++) {
-            k = k++;
-            l = ++l;
-        }
-        System.out.println(k);
-        System.out.println(l);
-    }
+//    int a = 1;
+//
+//    public void add(int b){
+//        b = b + 1;  //复制入参，备份加
+//    }
+//
+//    public static void main(String[] args) {
+//        Test t = new Test();
+//
+//        new Test().add(t.a);
+//        System.out.println(t.a);
+//    }
+
+
 }
